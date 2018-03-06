2e1869eae192de54b3530070190c6dbdc69d78e1
talhay <talhayassine90@gmail.com>
2018-03-06 16:58:49 +0100

    J'ai rajouté le deuxième exo

--- InputOutput/src/interfacePath/Chemins.java
+++ InputOutput/src/interfacePath/Chemins.java
@@ -1,17 +1,19 @@
 package interfacePath;
 
+import java.io.IOException;
 import java.nio.file.*;
 
 
-//******************************Exercice 1 : Prog 2 ****************************************
 
-/*Créez un programme qui crée un chemin (pas forcément vers un fichier qui existe) et qui affiche le
-résultat de l'appel aux méthodes suivantes sur le chemin :
-toString(), getFileName(), getName(0), getNameCount(), subpath(0,2),
-getParent(), getRoot()*/
 
 public class Chemins {
 	
+	//******************************Exercice 1 : Prog 2 ****************************************
+
+	/*Créez un programme qui crée un chemin (pas forcément vers un fichier qui existe) et qui affiche le
+	résultat de l'appel aux méthodes suivantes sur le chemin :
+	toString(), getFileName(), getName(0), getNameCount(), subpath(0,2),
+	getParent(), getRoot()*/
 	public static void main(String[] args) {
 		
 		Path p1 = Paths.get("D:\\Programme Files\\Git\\InputsOutputs-Java\\InputOutput\\test");
@@ -22,6 +24,22 @@ public class Chemins {
 		System.out.println(p1.getNameCount()); //Le nombre de dossier composant notre Path 
 		System.out.println(p1.getRoot()); //La racine de notre Path
 		System.out.println(p1.subpath(0, 2)); //Les sous dossier (hors la racine)
-	}
+	
 
+	//******************************Exercice 2 : Prog 2 ****************************************
+	/*Soit le chemin suivant : Path relative = Paths.get("unfichier.txt");
+	Créer un programme qui converti et affiche le chemin ci-dessus en : chemin absolu, en URI, et en
+	chemin réel. Dans chacun des cas, que remarquez-vous ? */
+	System.out.println("*************** Exo2 ************************************");
+	 Path relative = Paths.get("unfichier.txt");
+	 System.out.println(relative.toUri());
+	 System.out.println(relative.toAbsolutePath());
+	 try {
+		System.out.println(relative.toRealPath());
+	} catch (IOException e) {
+		
+		e.printStackTrace();
+		System.out.println(" Vérifier que le fichier bien existe");
+	}
+	}
 }
